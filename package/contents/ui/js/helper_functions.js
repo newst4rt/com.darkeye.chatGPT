a4325af8fec6e6707fbf88e0d5499bbe0330358a
newst4rt <105312719+newst4rt@users.noreply.github.com>
2024-03-28 08:34:08 +0100

    fix enter

--- package/contents/ui/js/helper_functions.js
+++ package/contents/ui/js/helper_functions.js
@@ -1,16 +1,12 @@
 document.userScripts={saveData:{},config:{}};
 
+
 document.userScripts.getMainInput = function() {
-		return document.querySelector('textarea#prompt-textarea');
+		return document.querySelector('textarea.m-0');
 }
 
 document.userScripts.getSendButton = function() {
-		console.log(document.querySelector('button[data-testid=send-button]'));
-		return document.querySelector('button[data-testid=send-button]');
-}
-
-document.userScripts.getStopButton = function() {
-		return document.querySelector('button[aria-label=Stop generating]');
+		return document.querySelector('textarea.m-0 + button.p-1');
 }
 
 
@@ -23,26 +19,30 @@ document.userScripts.setInputFocus = function () {
 }
 
 
-document.userScripts.setSendOnEnter = function () {
+function loadCSS() {
+   var styles = 'button { background-color: #212121; }';
+   var styleSheet = document.createElement("style");
+   styleSheet.innerText = styles;
+   document.head.appendChild(styleSheet);
+	console.log('loadCSS');
+}
 
+
+                
+
+document.userScripts.setSendOnEnter = function () {
 	let inputElement =  document.userScripts.getMainInput();
 	if(inputElement && !document.userScripts.saveData.oldOnPress) {
 		document.userScripts.saveData.oldOnPress = inputElement.onkeypress;
 		inputElement.onkeypress = function(e) {
-			// console.log('keypress : ' + e.keyCode);
-			if(document.userScripts.config.sendOnEnter) {
-				if(	!e.shiftKey && !e.ctrlKey )
-					switch(e.keyCode) {
-						case 13 : {
-								document.userScripts.getSendButton().click();
-								return false;
-						}
-						break;
-					}
+			if(	e.keyCode == 13  && 
+				!e.shiftKey && !e.ctrlKey && 
+				document.userScripts.config.sendOnEnter) {
+                     document.querySelector('[data-testid=send-button]').click();
+					return false;
 			}
 		}
 	}
-
 	console.log('setSendOnEnter');
 }
 
